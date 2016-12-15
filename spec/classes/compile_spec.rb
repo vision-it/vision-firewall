30ed980d743204a5273aa23b9618dd01680847e2
Markus Opolka <markus.opolka@iis.fraunhofer.de>
2016-12-15 12:24:33 +0100

    Extend rspec tests

--- spec/classes/compile_spec.rb
+++ spec/classes/compile_spec.rb
@@ -41,6 +41,35 @@ describe 'vision_firewall' do
         it { is_expected.to compile.with_all_deps }
       end
 
+      context 'no exported resources' do
+        it { expect(exported_resources).not_to contain_vision_firewall__forward('400 foobar') }
+      end
+
+    end
+  end
+end
+
+
+describe 'vision_firewall' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      let(:params) {{
+                      :location       => 'something',
+                      :forward_rules  => {},
+                    }}
+
+      context 'compile' do
+        it { is_expected.to compile.with_all_deps }
+      end
+
+      context 'no exported resources' do
+        it { expect(exported_resources).not_to contain_vision_firewall__forward('400 foobar') }
+      end
+
     end
   end
 end
