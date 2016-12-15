81727cc5c3bf2884d567f50e6cdb620bb8e0d437
Markus Opolka <markus.opolka@iis.fraunhofer.de>
2016-12-15 10:26:55 +0100

    Add conditional parameter if no rules should be applied

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,13 +13,17 @@
 
 class vision_firewall (
 
-  Hash $forward_rules,
-  String $location = $::location,
+  Optional[Hash] $forward_rules = undef,
+  String $location              = $::location,
 
 ) {
 
-  if ($location =~ /(int|dmz)Vm/) {
-    create_resources('@@vision_firewall::forward', $forward_rules)
+  if ($forward_rules) {
+
+    if ($location =~ /(int|dmz)Vm/) {
+      create_resources('@@vision_firewall::forward', $forward_rules)
+    }
+
   }
 
 }
