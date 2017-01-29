73ddfe01cdad796b1867829157c538198500efee
Markus Opolka <markus@martialblog.de>
2017-01-29 19:08:06 +0100

    Add beaker tests for iptables

--- spec/acceptance/default_spec.rb
+++ spec/acceptance/default_spec.rb
@@ -14,10 +14,15 @@ describe 'vision_firewall' do
     end
   end
 
-  # TODO Add tests for test rules
   describe iptables do
-    #it { should have_rule('-A INPUT -p tcp --dport 80 -j ACCEPT') }
-    #it { should have_rule('-P INPUT ACCEPT') }
+    it { should have_rule('-A INPUT -p icmp -m comment --comment "000 accept all ICMP" -j ACCEPT') }
+    it { should have_rule('-A INPUT -i lo -m comment --comment "001 accept all to lo interface" -j ACCEPT') }
+    it { should have_rule('-A INPUT -d 127.0.0.0/8 ! -i lo -m comment --comment "002 reject local traffic not on loopback interface" -j REJECT --reject-with icmp-port-unreachable') }
+    it { should have_rule('-A INPUT -m comment --comment "003 accept related established rules" -m state --state RELATED,ESTABLISHED -j ACCEPT') }
+    it { should have_rule('-A INPUT -p tcp -m multiport --dports 22 -m comment --comment "010 accept all SSH" -j ACCEPT') }
+    it { should have_rule('-A INPUT -p tcp -m multiport --ports 80 -m comment --comment "100 allow http access" -j ACCEPT') }
+    it { should have_rule('-A INPUT -p tcp -m multiport --ports 443 -m comment --comment "101 allow https access" -j ACCEPT') }
+    it { should have_rule('-A INPUT -p tcp -m comment --comment "999 drop all other requests" -j DROP') }
   end
 
 end
