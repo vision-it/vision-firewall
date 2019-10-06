4a49571e6a765720dbecab6d0fbc8fab079ffac5
Jack Henschel <jackdev@mailbox.org>
2019-10-06 09:57:51 +0200

    Cleanup firewall rules
    https://gitlab.cc-asp.fraunhofer.de/vision-it/puppet/control/issues/169

--- spec/acceptance/default_spec.rb
+++ spec/acceptance/default_spec.rb
@@ -27,10 +27,8 @@ describe 'vision_firewall' do
     it { is_expected.to have_rule('-A INPUT -p icmp -m comment --comment "000 accept all ICMP" -j ACCEPT') }
     it { is_expected.to have_rule('-A INPUT -i lo -m comment --comment "001 accept all to lo interface" -j ACCEPT') }
     it { is_expected.to have_rule('-A INPUT -d 127.0.0.0/8 ! -i lo -m comment --comment "002 reject local traffic not on loopback interface" -j REJECT --reject-with icmp-port-unreachable') }
-    it { is_expected.to have_rule('-A INPUT -m state --state RELATED,ESTABLISHED -m comment --comment "003 accept related established rules" -j ACCEPT') }
+    it { is_expected.to have_rule('-A INPUT -m state --state RELATED,ESTABLISHED -m comment --comment "004 accept related established rules" -j ACCEPT') }
 
-    it { is_expected.to have_rule('-A INPUT -p tcp -m multiport --dports 22 -m comment --comment "010 accept all SSH" -j ACCEPT') }
-    it { is_expected.to have_rule('-A INPUT -p tcp -m multiport --dports 25 -m comment --comment "011 accept all SMTP" -j ACCEPT') }
     it { is_expected.to have_rule('-A INPUT -p tcp -m multiport --dports 443 -m comment --comment "101 allow https access" -j ACCEPT') }
     it { is_expected.to have_rule('-A INPUT -m comment --comment "999 drop all other requests" -j DROP') }
   end
